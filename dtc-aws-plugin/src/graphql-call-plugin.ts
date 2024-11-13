65e79d1bbe15474f8a36953d9bef25e4e5907a61
salam <salamsuleymanov@gmail.com>
2024-11-13 13:51:59 +0100

    graphql call plugin

---
+++ dtc-aws-plugin/src/graphql-call-plugin.ts
@@ -0,0 +1,33 @@
+import extraAssert from '@cgauge/assert'
+import {isRecord} from '@cgauge/dtc'
+import {GraphQLClient} from 'graphql-request'
+
+type GraphQlCall = {url: string; query: string; variables: {}} & RequestInit
+
+const isGraphQlCall = (v: unknown): v is GraphQlCall => isRecord(v) && 'url' in v
+
+let response: Response
+
+export const act = async (args: unknown) => {
+  if (!isGraphQlCall(args)) {
+    return
+  }
+
+  const graphQLClient = new GraphQLClient(args.url, {
+    headers: {authorization: process.env.AUTHORIZATION_TOKEN as string},
+  })
+
+  response = await graphQLClient.request(args.query, args.variables)
+}
+
+export const assert = async (args: unknown) => {
+  if (!isRecord(args)) {
+    return
+  }
+
+  if (!args.graphql) {
+    return
+  }
+
+  extraAssert.objectContains(response, args.graphql)
+}

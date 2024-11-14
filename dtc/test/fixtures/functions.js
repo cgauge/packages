a7f28d9f4cde38e999fb57726d16fdb8865968c0
Salam Suleymanov <salamsuleymanov@gmail.com>
2024-11-14 14:10:18 +0100

    assert exception (#25)
    * assert exception
    
    * assert exception
    
    * bump version
    
    * bump version

--- dtc/test/fixtures/functions.js
+++ dtc/test/fixtures/functions.js
@@ -3,3 +3,7 @@ export const noArgs = () => true
 export const syncFunction = (args) => args
 
 export const asyncFunction = async (args) => args
+
+export const functionWithException = (__args) => {
+    throw new Error()
+}

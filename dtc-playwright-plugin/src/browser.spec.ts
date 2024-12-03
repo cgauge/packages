c798c4c80753dc97d3c316ceacad2bfec1dab8fe
Ab <219340+abdala@users.noreply.github.com>
2024-12-03 12:10:45 +0000

    Remove projectPath from filePath before loading testCases

--- dtc-playwright-plugin/src/browser.spec.ts
+++ dtc-playwright-plugin/src/browser.spec.ts
@@ -1,11 +1,11 @@
 import {executeTestCase, resolveConfig, loadTestCases} from '@cgauge/dtc'
 import {test} from '@playwright/test'
 
-const path = process.env.DTC_PLAYWRIGHT_PATH
+const projectPath = process.cwd()
+const path = process.env.DTC_PLAYWRIGHT_PATH?.replace(projectPath, '');
 const config = String(process.env.DTC_PLAYWRIGHT_CONFIG)
 
 const {loader, plugins, testRegex} = await resolveConfig(config)
-const projectPath = process.cwd()
 
 const testCaseExecutions = await loadTestCases(projectPath, loader, testRegex, path)
 

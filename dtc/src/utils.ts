dc8bc248919d6ab17a56b907acb6797c290e07b4
Ab C <219340+abdala@users.noreply.github.com>
2025-03-04 13:30:41 +0100

    Change retry logic

--- dtc/src/utils.ts
+++ dtc/src/utils.ts
@@ -1,36 +1,38 @@
 export const debug = (message: string) => process.env.DTC_DEBUG && process.stdout.write(`[DTC_DEBUG] ${message}\n`)
 export const info = (message: string) => process.env.DTC_DEBUG && process.stdout.write(`[DTC_INFO] ${message}\n`)
 export const warn = (message: string) => process.stdout.write(`[DTC_WARN] ${message}\n`)
-export const warnExit = (message: string) => { warn(message); process.exit(1) }
-export const error = (message: string) => { process.stdout.write(`[DTC_ERROR] ${message}\n`); process.exit(1) }
+export const warnExit = (message: string) => {
+  warn(message)
+  process.exit(1)
+}
+export const error = (message: string) => {
+  process.stdout.write(`[DTC_ERROR] ${message}\n`)
+  process.exit(1)
+}
 
 export const sleep = (ms: number): Promise<unknown> => new Promise((resolve) => setTimeout(resolve, ms))
 
 export const retry = async <T>(fn: () => Promise<T>, times = 0, seconds = 1): Promise<T> => {
   const errors: Error[] = []
 
-  // If times is 0 we call the function at least one time
-  if (times === 0) {
-    const result = await fn()
-    return result
-  }
-  
-  times++
-
-  for (let i = 0; i < times; i++) {
+  do {
     try {
       const result = await fn()
       return result
     } catch (e: any) {
       errors.push(e)
 
+      debug(`Retry ${times}`)
+
       await sleep(seconds * 1000)
+
+      times--
     }
-  }
+  } while (times > -1)
 
   debug(`Errors ${errors.map((e) => e.message)}`)
 
-  throw new Error(`Failed retrying ${times} times. ${errors[0].message}`)
+  throw errors[0]
 }
 
 export const merge = (to: any, from: any) => {

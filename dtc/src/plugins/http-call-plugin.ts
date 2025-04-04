43a9587cd7e3386d6916b49c3c80d1869b8d8fa8
Abe <219340+abdala@users.noreply.github.com>
2025-04-04 15:30:40 +0200

    Resolve body only one time (#53)
    * Resolve body only one time
    
    * Add response log

--- dtc/src/plugins/http-call-plugin.ts
+++ dtc/src/plugins/http-call-plugin.ts
@@ -1,9 +1,11 @@
 import nodeAssert from 'node:assert/strict'
 import extraAssert from '@cgauge/assert'
 import {is, union, optional, unknown, record, assert as typeAssert, diff} from '@cgauge/type-guard'
-import {info} from '../utils'
+import {debug, info} from '../utils'
 
 let response: Response | undefined
+let textResponse: string | undefined
+let jsonResponse: unknown | undefined
 
 const HttpCall = {
   url: String,
@@ -18,7 +20,9 @@ const HttpCallResponse = {http: union(String, record(String, unknown))}
 
 export const act = async (args: unknown): Promise<boolean> => {
   response = undefined
-  
+  textResponse = undefined
+  jsonResponse = undefined
+
   if (!is(args, HttpCall)) {
     const mismatch = diff(args, HttpCall)
     info(`(HTTP) Plugin declared but test declaration didn't match the act. Invalid ${mismatch[0]}`)
@@ -41,9 +45,15 @@ export const assert = async (args: unknown): Promise<boolean> => {
   }
 
   if (is(args.http, String)) {
-    nodeAssert.deepStrictEqual(await response?.text(), args.http)
+    textResponse = textResponse ?? (await response?.text())
+
+    debug(`(HTTP) Text response: ${textResponse}`);
+
+    nodeAssert.deepStrictEqual(textResponse, args.http)
   } else {
-    const jsonResponse = await response?.json()
+    jsonResponse = jsonResponse ?? (await response?.json())
+
+    debug(`(HTTP) JSON response: ${JSON.stringify(jsonResponse, null, 2)}`);
 
     typeAssert(jsonResponse, record(String, unknown))
 

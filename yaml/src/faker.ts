e545d624523289f4874e9dfdcb4bea37fb960e33
Ab C <219340+abdala@users.noreply.github.com>
2025-04-07 15:45:49 +0200

    Make brackets optional

--- yaml/src/faker.ts
+++ yaml/src/faker.ts
@@ -8,6 +8,10 @@ export const fakerType = new Type('!faker', {
       throw new Error('Invalid faker data.')
     }
 
+    if (data.indexOf('{{') === -1) {
+      data = `{{${data}}}`
+    }
+
     const value = faker.helpers.fake(data)
 
     return isNaN(Number(value)) ? value : parseFloat(value)

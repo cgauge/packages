656688048675c43117e2da5e1e013635174dae7a
Ab C <219340+abdala@users.noreply.github.com>
2025-04-08 22:35:09 +0200

    Fix function call

--- dtc-playwright-plugin/src/browser.spec.ts
+++ dtc-playwright-plugin/src/browser.spec.ts
@@ -7,14 +7,14 @@ const config = String(process.env.DTC_PLAYWRIGHT_CONFIG)
 
 const {loader, plugins, testRegex} = await resolveConfig(config)
 
-const testCaseExecutions = await loadTestCases(projectPath, loader, testRegex, path)
+const testCaseExecutions = await loadTestCases(projectPath)({loader, testRegex})(path)
 
 for (const testCaseExecution of testCaseExecutions) {
     if (testCaseExecution.testCase.use) {
         test.use(testCaseExecution.testCase.use)
     }
 
-    test(testCaseExecution.testCase.name, async ({page}) => {
+    test(testCaseExecution.testCase.name ?? testCaseExecution.filePath, async ({page}) => {
         testCaseExecution.testCase.timeout && test.setTimeout(testCaseExecution.testCase.timeout)
         await executeTestCase(testCaseExecution, plugins, {page})
     })

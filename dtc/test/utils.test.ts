98c508abf528e8260c5ec649fb22bccae4813daf
Abe <219340+abdala@users.noreply.github.com>
2025-04-10 13:39:48 +0200

    Add support for Array during object merging (#60)
    * Add support for Array during object merging
    
    * Bump version
    
    * Remove test.only
    
    * Clone object before merging it

--- dtc/test/utils.test.ts
+++ dtc/test/utils.test.ts
@@ -1,6 +1,6 @@
 import {test} from 'node:test'
 import assert from 'node:assert'
-import {retry} from '../src/utils'
+import {retry, merge} from '../src/utils'
 
 test('It execute at least one time', async () => {
   const response = await retry(() => Promise.resolve(true))
@@ -25,4 +25,34 @@ test('It retries when it fails', async () => {
   }, 1)
 
   assert.ok(response)
+});
+
+([
+  //objects
+  [{a: 'a'}, {a: 'b'}, {a: 'b'}],
+  [{a: 'a', b: 'c'}, {a: 'b'}, {a: 'b', b: 'c'}],
+  [{a: 'a'}, {a: 'b', b: 'c'}, {a: 'b', b: 'c'}],
+  [{a: {a: 'a'}}, {a: {a: 'b'}}, {a: {a: 'b'}}],
+  [{a: {a: 'a'}, b: 'c'}, {a: {a: 'b'}}, {a: {a: 'b'}, b: 'c'}],
+  //arrays
+  [[1], [2], [2]],
+  [[1], [2,3], [2,3]],
+  [[1,2], [3], [3,2]],
+  [[[1]], [[2]], [[2]]],
+  [[[1], 2], [[2]], [[2], 2]],
+  [[[1]], [[2], 3], [[2], 3]],
+  //mixed types
+  [{a: [1]}, {a: 1}, {a: 1}],
+  [{a: 1}, {a: [1]}, {a: [1]}],
+  [{a: {b: 'a'}}, {a: [1]}, {a: [1]}],
+  [[{a: 'a', b: 'c'}], [{a: 'b'}], [{a: 'b', b: 'c'}]],
+  //primitives
+  [1, 2, 2],
+  ['a', 'b', 'b'],
+]).forEach(([source, target, expected], index) => {
+  test(`It merge objects (${index})`, async () => {
+    const result = merge(source, target)
+
+    assert.deepStrictEqual(result, expected)
+  })
 })

092bf844405332d03ca9985558f67da7530f163f
salam <salamsuleymanov@gmail.com>
2025-06-27 12:26:47 +0200

    add redis mock plugin

--- dtc-redis-plugin/src/socket-mock.ts
+++ dtc-redis-plugin/src/socket-mock.ts
@@ -1,72 +1,31 @@
-import net from 'node:net'
+import { Socket } from 'node:net'
+import { Buffer } from 'node:buffer'
 
 export type WriteCallback = (error?: Error | null) => void
 
-export type WriteArgs =
-  | [chunk: unknown, callback?: WriteCallback]
-  | [chunk: unknown, encoding: BufferEncoding, callback?: WriteCallback]
-
-export type NormalizedSocketWriteArgs = [
-  chunk: any,
-  encoding?: BufferEncoding,
-  callback?: WriteCallback,
-]
-
-export function normalizeSocketWriteArgs(
-  args: WriteArgs
-): NormalizedSocketWriteArgs {
-  const normalized: NormalizedSocketWriteArgs = [args[0], undefined, undefined]
-
-  if (typeof args[1] === 'string') {
-    normalized[1] = args[1]
-  } else if (typeof args[1] === 'function') {
-    normalized[2] = args[1]
-  }
-
-  if (typeof args[2] === 'function') {
-    normalized[2] = args[2]
-  }
-
-  return normalized
-}
-
 export interface MockSocketOptions {
-  write: (
-    socket: net.Socket,
-    chunk: Buffer | string
-  ) => void
+  write: (socket: Socket, chunk: string | Buffer) => void
 }
 
-export class MockSocket extends net.Socket {
-  public connecting: boolean
-
-  constructor(protected readonly options: MockSocketOptions) {
+export class MockSocket extends Socket {
+  constructor(private readonly options: MockSocketOptions) {
     super()
-    this.connecting = false
-    this.connect()
+  }
 
-    this._final = (callback) => {
-      callback(null)
+  override write(buffer: string | Buffer, cb?: WriteCallback): boolean
+  override write(buffer: string | Buffer, encoding?: BufferEncoding, cb?: WriteCallback): boolean
+  override write(
+    buffer: string | Buffer,
+    encodingOrCb?: BufferEncoding | WriteCallback,
+    cb?: WriteCallback
+  ): boolean {
+    const callback = typeof encodingOrCb === 'function' ? encodingOrCb : cb
+    this.options.write(this, buffer)
+
+    if (callback) {
+      callback()
     }
-  }
-
-  public connect() {
-    return this
-  }
-
-  public write(...args: Array<unknown>): boolean {
-    const [chunk] = normalizeSocketWriteArgs(
-      args as WriteArgs
-    )
-    this.options.write(this, chunk)
+    
     return true
-  }
-
-  public end(...args: Array<unknown>) {
-    const [chunk] = normalizeSocketWriteArgs(
-      args as WriteArgs
-    )
-    this.options.write(this, chunk)
-    return super.end.apply(this, args as any)
   }
 }

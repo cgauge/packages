9580106f5d60998343227831755880521fb6ddb7
Ab C <219340+abdala@users.noreply.github.com>
2025-07-22 15:19:16 +0200

    Pass filePath to runner

--- dtc-playwright-plugin/src/playwright-runner.ts
+++ dtc-playwright-plugin/src/playwright-runner.ts
@@ -6,13 +6,13 @@ import * as url from 'url'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 export default (constructorArgs?: string[]) =>
-  async (testCaseExecutions: TestCaseExecution[], _plugins: string[], runnerArgs: string[], config?: string) => {
+  async (_testCaseExecutions: TestCaseExecution[], _plugins: string[], runnerArgs: string[], config?: string, filePath?: string) => {
     const args = ['playwright', 'test', ...runnerArgs,  ...constructorArgs ?? [], __dirname]
     const childProcess = spawnSync('npx', args, {
       stdio: 'inherit',
       env: {
         ...process.env,
-        ...(testCaseExecutions.length === 1 && {DTC_PLAYWRIGHT_PATH: testCaseExecutions[0].filePath}),
+        DTC_PLAYWRIGHT_PATH: filePath,
         DTC_PLAYWRIGHT_CONFIG: config,
       },
     })
